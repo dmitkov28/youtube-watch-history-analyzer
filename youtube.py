f2b3a36f709c3678eb8c4b2abed2ddaf4fab2989
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-03-18 09:22:39 +0200

    feat: add get video categories function

--- youtube.py
+++ youtube.py
@@ -7,3 +7,15 @@ async def get_video_metadata(client, video_id):
 
     if response["items"]:
         return response["items"]
+
+
+def get_video_categories(client, region_code="US"):
+    response = (
+        client.videoCategories().list(part="snippet", regionCode=region_code).execute()
+    )
+
+    categories = []
+    for item in response.get("items", []):
+        categories.append({"id": item["id"], "title": item["snippet"]["title"]})
+
+    return categories
