69142c1970e23176e4aa172ff267c1c2052d37c9
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-03-19 18:29:25 +0200

    feat: add avg videos per day indicator

--- dashboard.py
+++ dashboard.py
@@ -2,10 +2,15 @@ import plotly.graph_objects as go
 from dash import Dash, dcc, html
 
 from analyze import load_json_as_df
+from components import indicator
 
 df = load_json_as_df("./watch_history.json")
 app = Dash()
 
+total_videos_watched = len(df["video_id"].unique())
+
+df["date"] = df["timestamp"].dt.date
+avg_videos_per_day = df.groupby("date").size().mean()
 
 watched_videos_count = df.groupby(df["timestamp"].dt.to_period("W")).agg(
     {"video_id": "count"}
@@ -76,6 +81,13 @@ videos_fig.update_layout(
 app.layout = html.Div(
     [
         html.H1(children="YouTube Watch History", style={"textAlign": "center"}),
+        html.Div(
+            [
+                indicator(dcc, go, total_videos_watched, "Total Videos Watched"),
+                indicator(dcc, go, avg_videos_per_day, "Average Videos per Day"),
+            ],
+            style={"display": "flex", "justifyContent": "space-between", "maxHeight": "320px"},
+        ),
         html.Div(
             [
                 dcc.Graph(figure=watched_videos_count_chart),
