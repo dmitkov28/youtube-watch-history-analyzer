7bf3f40754211bf25d7535baaed774023f93075b
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-03-19 18:29:04 +0200

    refactor: remove id param

--- components/indicator.py
+++ components/indicator.py
@@ -3,7 +3,6 @@ from dash import dcc
 
 def indicator(dcc, go_module, value: int | float, title: str) -> dcc.Graph:
     graph = dcc.Graph(
-        id="gauge-chart",
         figure=go_module.Figure(
             go_module.Indicator(
                 mode="number",
