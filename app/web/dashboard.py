143b098cc3163fa65260d76025087994dfa8bc46
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-03-23 21:06:15 +0200

    feat: set up mantine

--- app/web/dashboard.py
+++ app/web/dashboard.py
@@ -1,11 +1,13 @@
 import dash
+import dash_mantine_components as dmc
 from dash import Dash, html
 
+app = Dash(__name__, use_pages=True, external_stylesheets=dmc.styles.ALL)
 
-app = Dash(__name__, use_pages=True)
-
-app.layout = html.Div(
-    [
-        dash.page_container,
-    ]
+app.layout = dmc.MantineProvider(
+    html.Div(
+        [
+            dash.page_container,
+        ]
+    )
 )
