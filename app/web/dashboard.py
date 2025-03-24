0d0e04b171daff29a3693c2f42fa2ea7347179c1
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-03-24 11:24:14 +0200

    feat: add dmc app shell

--- app/web/dashboard.py
+++ app/web/dashboard.py
@@ -1,13 +1,46 @@
 import dash
 import dash_mantine_components as dmc
-from dash import Dash, html
+from dash import Dash
 
 app = Dash(__name__, use_pages=True, external_stylesheets=dmc.styles.ALL)
 
-app.layout = dmc.MantineProvider(
-    html.Div(
-        [
-            dash.page_container,
-        ]
-    )
+layout = dmc.AppShell(
+    [
+        dmc.AppShellHeader(
+            dmc.Group(
+                [
+                    dmc.Burger(id="burger", size="sm", hiddenFrom="sm", opened=False),
+                    dmc.Title("YouTube Analytics", c="blue"),
+                ],
+                h="100%",
+                px="md",
+            )
+        ),
+        dmc.AppShellNavbar(
+            id="navbar",
+            children=[
+                dmc.NavLink(
+                    label="Videos",
+                    href="/videos",
+                ),
+                dmc.NavLink(
+                    label="Channels",
+                    href="/channels",
+                ),
+            ],
+            p="md",
+        ),
+        dmc.AppShellMain([dash.page_container]),
+    ],
+    header={"height": 60},
+    padding="md",
+    navbar={
+        "width": 300,
+        "breakpoint": "sm",
+        "collapsed": {"mobile": True},
+    },
+    id="appshell",
 )
+
+
+app.layout = dmc.MantineProvider(layout)
