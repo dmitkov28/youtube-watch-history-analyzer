8462a02bd2879fd54c5ad7e49ae76ab207bb35eb
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-03-24 17:51:54 +0200

    feat: add heatmap component

--- app/web/components/__init__.py
+++ app/web/components/__init__.py
@@ -2,5 +2,6 @@ from .bar_chart import bar_chart
 from .indicator import indicator
 from .line_chart import line_chart
 from .pie_chart import pie_chart
+from .heatmap import heatmap
 
-__all__ = ["bar_chart", "indicator", "line_chart", "pie_chart"]
+__all__ = ["bar_chart", "indicator", "line_chart", "pie_chart", "heatmap"]
---
+++ app/web/components/heatmap.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from dash import dcc
+
+
+def heatmap(px, df: pd.DataFrame, title: str, legend_label: str):
+    fig = px.density_heatmap(
+        df,
+        x="hour",
+        y="day_of_week",
+        z="count",
+        color_continuous_scale="Blues",
+        title=title,
+        labels={"hour": "Hour of Day", "day_of_week": "Day of Week", "count": "Videos Watched"},
+    )
+    fig.update_layout(coloraxis_colorbar_title=legend_label)
+    return dcc.Graph(figure=fig)
