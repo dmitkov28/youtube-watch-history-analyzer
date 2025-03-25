eca50ff6ce71f8937b135270fa3434d1331e2cbc
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-03-25 17:24:00 +0200

    style: run ruff

--- app/web/components/heatmap.py
+++ app/web/components/heatmap.py
@@ -10,7 +10,11 @@ def heatmap(px, df: pd.DataFrame, title: str, legend_label: str):
         z="count",
         color_continuous_scale="Blues",
         title=title,
-        labels={"hour": "Hour of Day", "day_of_week": "Day of Week", "count": "Videos Watched"},
+        labels={
+            "hour": "Hour of Day",
+            "day_of_week": "Day of Week",
+            "count": "Videos Watched",
+        },
     )
     fig.update_layout(coloraxis_colorbar_title=legend_label)
     return dcc.Graph(figure=fig)
