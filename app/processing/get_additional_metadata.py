da1a61bb40c437a59d71cae8611f9339d34f84d9
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-04-01 10:38:32 +0300

    feat: fetch youtube channels metadata

--- app/processing/get_additional_metadata.py
+++ app/processing/get_additional_metadata.py
@@ -8,7 +8,7 @@ from typing import Any, Dict, List
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
-from app.processing.youtube import get_video_metadata
+from app.processing.youtube import get_channel_metadata, get_video_metadata
 
 
 def save_to_json(data: List[Dict[str, str]], file_name: str) -> None:
@@ -22,13 +22,18 @@ def read_data(file_name: str) -> List[Dict]:
     return videos
 
 
-def process_data(data: List[Dict]) -> List[str]:
+def process_video_ids(data: List[Dict]) -> List[str]:
     video_ids = [str(video.get("video_id")) for video in data]
     return video_ids
 
 
-def save_data(data: List[Any]):
-    with open("youtube_data.json", "w") as f:
+def process_channel_ids(data: List[Dict]) -> List[str]:
+    channel_ids = [str(video.get("channel_url")) for video in data]
+    return channel_ids
+
+
+def save_data(data: List[Any], file_path: str):
+    with open(file_path, "w") as f:
         json.dump(data, f)
 
 
@@ -37,6 +42,16 @@ async def get_videos_metadata(client, video_ids: List[str], batch_size=50):
     for i in range(0, len(video_ids), batch_size):
         batch_ids = ",".join(video_ids[i : i + batch_size])
         task = asyncio.create_task(get_video_metadata(client, batch_ids))
+        tasks.append(task)
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+async def get_channels_metadata(client, channel_ids: List[str], batch_size=50):
+    tasks = []
+    for i in range(0, len(channel_ids), batch_size):
+        batch_ids = ",".join(channel_ids[i : i + batch_size])
+        task = asyncio.create_task(get_channel_metadata(client, batch_ids))
         tasks.append(task)
     results = await asyncio.gather(*tasks)
     return results
@@ -55,6 +70,26 @@ if __name__ == "__main__":
     client = build("youtube", "v3", developerKey=API_KEY)
 
     watch_history_videos = read_data(args.file_path)
-    watch_history_video_ids = list(set(process_data(watch_history_videos)))
-    youtube_data = asyncio.run(get_videos_metadata(client, watch_history_video_ids))
-    save_data(list(itertools.chain(*youtube_data)))
+    watch_history_video_ids = list(set(process_video_ids(watch_history_videos)))
+    watch_history_channel_ids = list(
+        set(
+            [
+                channel.split("/")[-1]
+                for channel in process_channel_ids(watch_history_videos)
+            ]
+        )
+    )
+    youtube_videos_data = asyncio.run(
+        get_videos_metadata(client, watch_history_video_ids)
+    )
+    save_data(
+        list(itertools.chain(*youtube_videos_data)), file_path="youtube_data.json"
+    )
+
+    youtube_channels_data = asyncio.run(
+        get_channels_metadata(client, watch_history_channel_ids)
+    )
+    save_data(
+        list(itertools.chain(*youtube_channels_data)),
+        file_path="youtube_channels_data.json",
+    )
--- app/processing/youtube.py
+++ app/processing/youtube.py
@@ -19,3 +19,27 @@ def get_video_categories(client, region_code="US"):
         categories.append({"id": item["id"], "title": item["snippet"]["title"]})
 
     return categories
+
+
+async def get_channel_metadata(client, channel_id):
+    request = client.channels().list(part="snippet,statistics", id=channel_id)
+    response = request.execute()
+    channels = []
+
+    for channel in response.get("items", []):
+        title = channel["snippet"]["title"]
+        description = channel["snippet"]["description"]
+        subscribers = channel["statistics"]["subscriberCount"]
+        videos = channel["statistics"]["videoCount"]
+        views = channel["statistics"]["viewCount"]
+
+        channels.append(
+            {
+                "title": title,
+                "description": description,
+                "subscribers": subscribers,
+                "total_videos": videos,
+                "total_views": views,
+            }
+        )
+    return channels
